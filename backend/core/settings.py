bc1a9fb245e40a7ca77e55da4b84df7b1c143cb2
Sijian Xuan <sijian.xuan@gmail.com>
2026-05-04 14:45:23 -0700

    update db to see data

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -76,12 +76,11 @@ WSGI_APPLICATION = "core.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3', # use db for db file to persist data
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
